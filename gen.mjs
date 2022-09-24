409d0a95c3c9deaf6f691aa5d9aca8bf2076959f
FK::yanlong <qiuyanlong2016@gmail.com>
2022-09-24 21:22:30 +0800

    test

--- gen.mjs
+++ gen.mjs
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
-import { $ } from "zx";
+import { $,fetch,fs} from "zx";
 await $`cat package.json | grep name`
+
+const url = "https://github.com/9a-aaaaaaaa/blog/blob/master/README.md";
+const resp = await fetch(url);
+const body = await resp.text();
+
+
+
